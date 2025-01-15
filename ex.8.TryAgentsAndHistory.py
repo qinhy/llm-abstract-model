4c3eadb4e2901b9cb651da9fb50e038c186c2858
koki.chin <545705507@qq.com>
2025-01-15 10:11:11 +0900

    Update ex.8.TryAgentsAndHistory.py

--- ex.8.TryAgentsAndHistory.py
+++ ex.8.TryAgentsAndHistory.py
@@ -1,11 +1,7 @@
-import datetime
-import json
-import math
 from typing import List, Optional
 from uuid import uuid4
 from pydantic import BaseModel, Field
 from LLMAbstractModel.LLMsModel import LLMsStore, Model4LLMs
-from LLMAbstractModel.utils import RegxExtractor
 
 # Node model for each node in the memory treefrom typing import List, Optional
 class TextContentNode(BaseModel):
@@ -60,6 +56,10 @@ class TextContentNode(BaseModel):
         self.children.append(child)
         child.parent_id = self.id
         child.depth = self.depth + 1
+        if self.is_root():
+            child._root_node = self
+        elif self._root_node:
+            child._root_node = self._root_node
 
     def get_parent(self) -> Optional['TextContentNode']:
         return self.get_node(self.parent_id) if self.parent_id else None
@@ -89,7 +89,6 @@ class HistoryAssistantAgent(BaseModel):
     # text_embedding: Model4LLMs.AbstractEmbedding = None
     history_root: TextContentNode = TextContentNode()
     history_last: TextContentNode = None
-    # history_last_k: int = Field(default=5, ge=1, description="Number of last history to retrieve.")
 
     def print_tree(self, node: Optional[TextContentNode] = None, level: int = 0, is_print=True):
         res = ''
@@ -109,45 +108,45 @@ class HistoryAssistantAgent(BaseModel):
         return self.history_last
 
     def add_history(self, content: str, role:str='user'):
-        p = self.get_last_history().get_parent()
-        if p is None:p = self.get_last_history()
         last = TextContentNode(content=role+'@@@@'+content)
-        p.add(last)
+        self.get_last_history().add(last)
         self.history_last = last
     
     def history_retrieval(self, last_k: int=4):
         p = self.get_last_history()
-        return p.children[-last_k:]
+        res = p.parents() + [p]
+        return [i for i in res if not i.is_root()][-last_k:]
 
     def __call__(self, qustion: str, system_prompt:str=None, last_k: int=4, print_history=True) -> str:
-        
-        if print_history:
-            print("############ For Debug ##############")
-            self.print_history()
-            print("#####################################")
-            print()
-
-        history = self.history_retrieval(last_k)
         tmp = self.llm.system_prompt
         self.llm.system_prompt = system_prompt
-        response = self.llm([
-                {'role':h.content.split('@@@@')[0],
+
+        history = self.history_retrieval(last_k)
+        msgs = [{'role':h.content.split('@@@@')[0],
                  'content':'@@@@'.join(h.content.split('@@@@')[1:])} for h in history
-            ]+{'role':'user','content':qustion})
+            ]+[{'role':'user','content':qustion}]
+        if print_history:
+            print("############ For Debug ##############")
+            self.print_tree()
+            print("#####################################")
+            [print(i) for i in msgs]
+            print("#####################################")
+        response = self.llm(msgs)
         self.add_history(qustion)
         self.add_history(response,'assistant')
+
         self.llm.system_prompt = tmp
         return response
 
 # Functions for secure data storage and retrieval using RSA key pair
 def save_history_agent(store: LLMsStore, root_node: TextContentNode):
     store.set('history', root_node.model_dump())
-    store.dump_RSA('./tmp/store.rjson', './tmp/public_key.pem',True)
+    store.dump_RSA('./tmp/TryAgentsAndHistory.rjson', './tmp/public_key.pem',True)
 
 def load_history_agent():
     # Load stored RSA-encrypted data and initialize the agent
     store = LLMsStore()
-    store.load_RSA('./tmp/store.rjson', './tmp/private_key.pem')
+    store.load_RSA('./tmp/TryAgentsAndHistory.rjson', './tmp/private_key.pem')
     
     # Retrieve saved model instances
     llm = store.find_all('ChatGPT4oMini:*')[0]
@@ -156,7 +155,12 @@ def load_history_agent():
     agent = HistoryAssistantAgent(history_root=store.get('history'), llm=llm)
     return agent, store
 
-# Example usage of saving and loading the memory agent
-# save_memory_agent(store, agent.memory_root)
-# agent, store = load_memory_agent()
+# Example usage of saving and loading the history agent
+# save_history_agent(store, agent.history_root)
+# agent, store = load_history_agent()
 # print(agent("Welcome back! What's planned for today?"))
+
+store = LLMsStore()
+vendor = store.add_new_openai_vendor(api_key='OPENAI_API_KEY')
+llm = store.add_new_chatgpt4omini(vendor_id=vendor.get_id(), temperature=0.7)
+agent = HistoryAssistantAgent(llm=llm)
